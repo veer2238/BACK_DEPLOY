5e4d62058090c71041cfee7b7677fc2448ffdcc7
veer2238 <veer2238rajput@gmail.com>
2024-09-27 11:14:37 +0530

    index

---
+++ index.js
@@ -0,0 +1,15 @@
+//index.js
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+console.log("server connected")
+})
+
+//Run the app with the following command
+//node app.js
